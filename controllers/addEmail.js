2b5491b8436c46f0da65a9c5e59a9f6b7837a951
Samprad <sampraaddas2503@gmail.com>
2026-01-08 11:40:37 +0530

    initial commit

---
+++ controllers/addEmail.js
@@ -0,0 +1,8 @@
+import { sql } from "../index.js";
+
+export async function addEmail(req, res) {
+  const { name, email, contestId, contestTime } = req.body;
+
+  await sql`INSERT INTO users (name, email, contest_id, contest_time) VALUES (${name}, ${email}, ${contestId}, ${contestTime})`;
+  res.status(200).json({ message: "Email added successfully" });
+}
